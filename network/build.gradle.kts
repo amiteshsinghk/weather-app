85c48228111a4c583f6f93dfb1a2b949d1ef27f5
Amitesh Singh <amiteshsinghk@gmail.com>
2024-09-19 16:03:56 +0530

    netwrok module added.

---
+++ network/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.amitesh.network"
+    compileSdk = Config.compileSdk
+
+    defaultConfig {
+        minSdk = Config.minSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Config.java
+        targetCompatibility = Config.java
+    }
+    kotlinOptions {
+        jvmTarget = Config.jvmTarget
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
